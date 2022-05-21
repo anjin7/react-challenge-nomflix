858c68af7fb443b01d4b5a3e060431e4677a457c
_An_Jin_ <aji0719@gmail.com>
2022-05-21 20:49:18 +0900

    animation

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,16 +1,5 @@
 import "styled-components";
 
 declare module "styled-components" {
-  export interface DefaultTheme {
-    red: string;
-    black: {
-      veryDark: string;
-      darker: string;
-      lighter: string;
-    };
-    white: {
-      darker: string;
-      lighter: string;
-    };
-  }
+  export interface DefaultTheme {}
 }
