123a64d71acaa86fed90d14a0d13fa8d55722244
_An_Jin_ <aji0719@gmail.com>
2022-05-24 23:40:16 +0900

    1.14.3

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,13 +1,43 @@
+import { useQuery } from "react-query";
+import styled from "styled-components";
 import { useLocation } from "react-router";
+import { getMovies, IGetMoviesResult } from "../api";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Wrapper = styled.div`
+  background: black;
+  padding-bottom: 200px;
+`;
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 
 function Search() {
   const location = useLocation();
   const keyword = new URLSearchParams(location.search).get("keyword");
   console.log(keyword);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  console.log(data?.results);
+  const SearchMovie = (keyword: string) => {
+    
+  };
   return (
-    <h2>
-      {keyword}
-    </h2>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <h2>
+          Search Result : {keyword}
+        </h2>
+      )}
+    </Wrapper>    
   )
 }
 export default Search;
